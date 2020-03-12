45eedea3f1b0dfef613d966466bb816a12f0eab3
vb <vsbombyk@gmail.com>
2020-03-12 17:47:45 +0200

    db is up. keep working...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
+import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: "localhost",
+      port: 5432,
+      username: "root",
+      password: "root",
+      database: "users_db",
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule
+  ],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
